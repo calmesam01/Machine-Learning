21afb386144b6b1fca10807172de2de25d98bde4
calmesam01 <sanyamsareen01@gmail.com>
2019-11-14 19:09:34 -0500

    Split dataset into train data and test data

--- 1. Data Preprocessing/data_preprocessing.py
+++ 1. Data Preprocessing/data_preprocessing.py
@@ -26,3 +26,10 @@ X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+#Splitting the data into test and train data
+#cross_validation changed to model_selection
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+
