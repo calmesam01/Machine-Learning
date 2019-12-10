d1268147489957a8eaec587980722e8b9e99e137
calmesam01 <sanyamsareen01@gmail.com>
2019-12-09 19:12:50 -0500

    Fitting the linear and polynomial models to the data

--- Regression/Polynomial Regression/polynomial_regression.py
+++ Regression/Polynomial Regression/polynomial_regression.py
@@ -20,4 +20,17 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
 
