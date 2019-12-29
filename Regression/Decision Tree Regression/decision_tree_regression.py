1852d2219400e330c66b30688eddf84068e0241a
calmesam01 <sanyamsareen01@gmail.com>
2019-12-28 22:30:07 -0500

    Fit the model and predict results

--- Regression/Decision Tree Regression/decision_tree_regression.py
+++ Regression/Decision Tree Regression/decision_tree_regression.py
@@ -22,4 +22,20 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
 
+# Predicting a new result
+y_pred = regressor.predict(np.array([[6.5]]))
+
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
